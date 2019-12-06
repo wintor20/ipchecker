1e278a9358b3b31a98ec219c1c8babcfe0bf8ef2
Дмитрий Ромашко <wintor20@gmail.com>
2019-12-06 16:01:46 +1000

    test data

--- sql/3.sql
+++ sql/3.sql
@@ -11,9 +11,18 @@ $$ LANGUAGE sql;
 -- mock data
 INSERT INTO conn_log (user_id, ip_addr, ts)
 SELECT
-       rand(10000),
+       rand(100000),
        rand(255)||'.'||rand(255)||'.'||rand(255)||'.'||rand(255),
        now() + (rand(100)::varchar||'h')::interval
-FROM generate_series(1, 50);
+FROM generate_series(1, 100000000);
+
+INSERT INTO conn_log (user_id, ip_addr, ts)
+VALUES
+(1, '1.1.1.1', now()),
+(1, '2.2.2.2', now()),
+(2, '1.1.1.1', now()),
+(2, '2.2.2.2', now())
+ON CONFLICT DO NOTHING;
+
 
 COMMIT;
